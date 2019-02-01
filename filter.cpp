700a32f64c257e026b7b294b8e18409ba8c77dd5
Chianghui-Wong <uangjianghui@qq.com>
2019-01-31 23:53:49 -0800

    finish

--- filter.cpp
+++ filter.cpp
@@ -12,13 +12,30 @@ Filter::Filter(const std::string& filename) {
 
 void Filter::read(std::string filename) {
 	// FIXME
+	std::ifstream infile(filename);
+	infile >> width >> height;
+	for(int i=0; i < width * height; i++) {
+		float f;
+		infile >> f;
+		kernel.push_back(f);
+	}
+	infile.close();
+	normalize();
 }
 
 void Filter::normalize() {
 	// FIXME
+	float sum = 0;
+	for (int i = 0; i< width * height; i++) {
+		sum += kernel[i];
+	}
+	for (int i = 0; i< width * height; i++) {
+		kernel[i]/=sum;
+	}
 }
 
 const float& Filter::at(int x, int y) const {
 	// FIXME
+	return kernel[y*width + x];
 }
 
